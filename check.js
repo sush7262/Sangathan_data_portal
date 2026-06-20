a43a74a6c335606c40d6d19ba670e1c54302dff2
Sushmit_Nakhate <susmitnakhate6207@gmail.com>
2026-06-20 16:27:08 +0530

    Final Changes

--- check.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const code = fs.readFileSync('app-data.js', 'utf8');
-console.log('Total file length:', code.length);
-const match = code.match(/"b64":\s*"([^"]+)"/);
-if (match) {
-    console.log('b64 string length:', match[1].length);
-} else {
-    console.log('b64 string not found!');
-}
--- replace.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('app-data.js', 'utf8');
-code = code.replace(/अन्य नगर/g, 'नगर');
-fs.writeFileSync('app-data.js', code, 'utf8');
-console.log('Replaced successfully');
